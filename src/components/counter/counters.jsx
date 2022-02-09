2e6e5b87bcc39c2c0db18a927244644d787139fa
Charlemagne Hernani <chlemagne@gmail.com>
2022-02-10 09:38:44 +1100

    Pass value and children to component

--- src/components/counter/counters.jsx
+++ src/components/counter/counters.jsx
@@ -4,18 +4,24 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
+      { id: 1, value: 0, disabled: false },
+      { id: 2, value: 5, disabled: false },
+      { id: 3, value: 100, disabled: false },
     ],
   };
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} />
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            disabled={counter.disabled}
+          >
+            <b>Counter #{counter.id}</b>
+          </Counter>
         ))}
+        <Counter disabled>Disabled Counter</Counter>
       </div>
     );
   }
